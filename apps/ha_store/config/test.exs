3c0763f57c186ffba604c82e043609d9389fc9c5
Mads Hargreave <mads.hargreave@gmail.com>
2019-01-03 18:29:12 +0100

    Added ingestion context

--- apps/ha_store/config/test.exs
+++
@@ -1,9 +0,0 @@
-use Mix.Config
-
-config :ha_store, HaStore.Repo,
-  adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
-  database: "harvest_dev",
-  hostname: "localhost",
-  pool_size: 10
--- apps/ha_store/lib/ha_store/support/dispatcher.ex
+++
@@ -1,7 +0,0 @@
-defmodule HaStore.Dispatcher do
-  @moduledoc """
-  Dispatcher for domain events
-  """
-  use HaSupport.Dispatcher,
-    adapter: {HaSupport.Dispatcher.StreamImpl, stream: "records", name: :redix_store}
-end
