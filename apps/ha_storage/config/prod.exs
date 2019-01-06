9181a864016e9580fb434592457e803270708e99
Mads Hargreave <mads.hargreave@gmail.com>
2019-01-06 19:51:37 +0100

    Asd

--- apps/ha_storage/config/prod.exs
+++ apps/ha_storage/config/prod.exs
@@ -1,1 +1,10 @@
 use Mix.Config
+
+# Configure your database
+config :ha_storage, HaStorage.Hashes.HashStore.Postgres.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: "postgres",
+  password: "XXXXXXXX",
+  database: "hello_prod",
+  socket_dir: "/tmp/cloudsql/[CONNECTION-NAME]",
+  pool_size: 15
