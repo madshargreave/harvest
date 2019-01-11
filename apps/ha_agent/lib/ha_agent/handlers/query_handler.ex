25c1992e9def4c7356512ca389cba99808d21ae8
Mads Hargreave <mads.hargreave@gmail.com>
2019-01-11 13:40:14 +0100

    Dockerfile

--- apps/ha_agent/lib/ha_agent/handlers/query_handler.ex
+++ apps/ha_agent/lib/ha_agent/handlers/query_handler.ex
@@ -1,40 +1,40 @@
-# defmodule HaAgent.Handlers.QueryHandler do
-#   @moduledoc """
-#   Runs and schedules queries
-#   """
-#   use GenConsumer, otp_app: :ha_agent
+defmodule HaAgent.Handlers.QueryHandler do
+  @moduledoc """
+  Runs and schedules queries
+  """
+  use GenConsumer, otp_app: :ha_agent
 
-#   alias ExdStreams.Api.Commands.SelectCommand
-#   alias HaSupport.DomainEvent
-#   alias HaCore.Jobs.Job
+  alias ExdStreams.Api.Commands.SelectCommand
+  alias HaSupport.DomainEvent
+  alias HaCore.Jobs.Job
 
-#   @impl true
-#   def handle_event(%DomainEvent{
-#     type: :job_created,
-#     data: job
-#   } = event) do
-#     context = %{id: event.actor_id}
+  @impl true
+  def handle_event(%DomainEvent{
+    type: :job_created,
+    data: job
+  } = event) do
+    context = %{id: event.actor_id}
 
-#     command =
-#       %SelectCommand{
-#         table: job.destination_id,
-#         query: struct(Exd.AST.Program, job.configuration.ast),
-#         meta: %{
-#           table_id: job.destination_id,
-#           temporary: !job.destination.saved,
-#           job_id: job.id,
-#           event: event
-#         }
-#       }
-#     ExdStreams.connect(context)
-#     ExdStreams.run(context, command)
-#     ExdStreams.close(context)
-#     :ok
-#   end
+    command =
+      %SelectCommand{
+        table: job.destination_id,
+        query: struct(Exd.AST.Program, job.configuration.ast),
+        meta: %{
+          table_id: job.destination_id,
+          temporary: !job.destination.saved,
+          job_id: job.id,
+          event: event
+        }
+      }
+    ExdStreams.connect(context)
+    ExdStreams.run(context, command)
+    ExdStreams.close(context)
+    :ok
+  end
 
-#   @impl true
-#   def handle_event(_) do
-#     :ok
-#   end
+  @impl true
+  def handle_event(_) do
+    :ok
+  end
 
-# end
+end
