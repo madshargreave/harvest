42908cf73216dd38408ccc71525f806ab66b7291
Mads Hargreave <mads.hargreave@gmail.com>
2019-01-15 16:48:01 +0100

    asd

---
+++ apps/ha_core/priv/ecto_impl/migrations/20190115154538_add_status_to_records.exs
@@ -0,0 +1,13 @@
+defmodule HaCore.Repo.EctoImpl.Migrations.AddStatusToRecords do
+  use Ecto.Migration
+
+  def change do
+    alter table(:records) do
+      add :table_id, references(:tables, type: :uuid), null: false
+      add :status, :string, null: false
+      add :ts, :naive_datetime, null: false
+      add :previous, :map
+    end
+  end
+
+end
